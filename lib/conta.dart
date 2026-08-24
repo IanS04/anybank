7954b55dd6b3f7891a5fd46f8801c91756333652
IanS04 <107213634+IanS04@users.noreply.github.com>
2026-08-24 20:59:46 -0300

    Create Conta class for managing account balance

---
+++ lib/conta.dart
@@ -0,0 +1,18 @@
+class Conta {
+  String titular;
+  double _saldo;
+
+  Conta(this.titular, this._saldo);
+
+  void receber(double valor) {
+  _saldo += valor;
+  }
+
+  void enviar(double valor) {
+  if(_saldo >= valor) {
+    _saldo -= valor;
+    }else{
+      print("saldo insuficiente");
+    }
+  }
+}
