bdb3bce60fad34480b00b6559b1f30fef783588f
IanS04 <107213634+IanS04@users.noreply.github.com>
2026-08-24 20:41:15 -0300

    Implement money transfer functions in anybank.dart
    Added functions to receive and send money between accounts.

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -12,6 +12,27 @@ void main() {
   
   contaRoberta.saldo = 15000;
   print(contaRoberta.saldo);
+
+  receber(contaMatheus, 1500);
+
+  print(contaMatheus.titular);
+  print(contaMatheus.saldo);
+
+  enviar(contaMatheus, 6500);
+  print(contaMatheus.titular);
+  print(contaMatheus.saldo);
+}
+
+void receber(Conta conta, double valor) {
+  conta.saldo += valor;
+}
+
+void enviar(Conta conta, double valor) {
+  if(conta.saldo >= valor) {
+    conta.saldo -= valor;
+    }else{
+      print("saldo insuficiente");
+    }
 }
 
 class Conta {
@@ -20,3 +41,4 @@ class Conta {
 
   Conta(this.titular, this.saldo);
 }
+
