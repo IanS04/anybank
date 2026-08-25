87752e2f9501c27c23f41cc99d3f0a0c18e17895
IanS04 <107213634+IanS04@users.noreply.github.com>
2026-08-24 21:07:40 -0300

    Add valorSaldo method to Conta class

--- lib/conta.dart
+++ lib/conta.dart
@@ -6,13 +6,19 @@ class Conta {
 
   void receber(double valor) {
   _saldo += valor;
+  valorSaldo();
   }
 
   void enviar(double valor) {
   if(_saldo >= valor) {
     _saldo -= valor;
+    valorSaldo();
     }else{
       print("saldo insuficiente");
     }
+  }
+
+  void valorSaldo() {
+    print("O saldo atual de $titular, é: R\$$_saldo");
   }
 }
