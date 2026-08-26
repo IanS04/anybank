9f89be1ba4fb7c4689379632e83c3f728b684fc6
IanS04 <107213634+IanS04@users.noreply.github.com>
2026-08-25 22:13:31 -0300

    Add ContaCorrente and ContaPoupanca classes

--- lib/conta.dart
+++ lib/conta.dart
@@ -22,3 +22,11 @@ class Conta {
     print("O saldo atual de $titular, é: R\$$_saldo");
   }
 }
+
+class ContaCorrente extends Conta{
+  ContaCorrente(super.titular, super._saldo);
+}
+
+class ContaPoupanca extends Conta {
+  ContaPoupanca(super.titular, super._saldo);
+}
