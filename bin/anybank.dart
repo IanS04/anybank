4d32f5eb8cee0f1d05e33ffca0cffc115df14947
IanS04 <107213634+IanS04@users.noreply.github.com>
2026-08-25 22:12:58 -0300

    Add ContaCorrente and ContaPoupanca instances
    Added ContaCorrente and ContaPoupanca instances for Welder and Ian, respectively. Included method calls for saldo and transactions.

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -3,6 +3,8 @@ import 'package:anybank/conta.dart';
 void main() {
   Conta contaMatheus = Conta("Matheus", 5000);
   Conta contaRoberta = Conta("Roberta", 10000);
+  ContaCorrente contaWelder = ContaCorrente("Welder", 50000);
+  ContaPoupanca contaIan = ContaPoupanca("Ian", 50000);
 
   List<Conta> contas = <Conta> [contaMatheus, contaRoberta];
   for (Conta conta in contas) {
@@ -13,5 +15,11 @@ void main() {
   contaRoberta.receber(5000);
   contaMatheus.receber(1500);
   contaMatheus.enviar(3000);
+
+  contaWelder.valorSaldo();
+  contaWelder.receber(500);
+
+  contaIan.valorSaldo();
+  contaIan.enviar(100);
 
 }
