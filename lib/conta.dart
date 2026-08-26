d65c3b58a9f6f50bfd2c1b5f4ad537b3944719c0
IanS04 <107213634+IanS04@users.noreply.github.com>
2026-08-25 23:44:06 -0300

    Add loan feature to ContaCorrente and update methods

--- lib/conta.dart
+++ lib/conta.dart
@@ -24,9 +24,31 @@ class Conta {
 }
 
 class ContaCorrente extends Conta{
+  double emprestimo = 2000;
+
   ContaCorrente(super.titular, super._saldo);
+
+  @override
+  void enviar(double valor) {
+    if(_saldo + emprestimo >= valor && _saldo < valor) {
+      _saldo = _saldo + emprestimo;
+      _saldo -= valor;
+      valorSaldo();
+    } else if (_saldo >= valor && _saldo + emprestimo > valor) {
+      _saldo -= valor;
+      valorSaldo();
+    } else {
+      print("saldo insuficiente");
+    }
+  }
 }
 
 class ContaPoupanca extends Conta {
+  double rendimento = 0.05;
+
   ContaPoupanca(super.titular, super._saldo);
+
+  void calculaRendimento() {
+    _saldo += _saldo * rendimento;
+  }
 }
