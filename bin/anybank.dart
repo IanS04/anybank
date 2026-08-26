ecd0af9311c340aff379115ad6ba236ba5068b20
IanS04 <107213634+IanS04@users.noreply.github.com>
2026-08-25 23:43:41 -0300

    Modify account initial balances and transaction amounts
    Updated account balances for ContaCorrente and ContaPoupanca. Adjusted transaction amounts for contaMatheus and contaWelder.

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -3,8 +3,8 @@ import 'package:anybank/conta.dart';
 void main() {
   Conta contaMatheus = Conta("Matheus", 5000);
   Conta contaRoberta = Conta("Roberta", 10000);
-  ContaCorrente contaWelder = ContaCorrente("Welder", 50000);
-  ContaPoupanca contaIan = ContaPoupanca("Ian", 50000);
+  ContaCorrente contaWelder = ContaCorrente("Welder", 5000);
+  ContaPoupanca contaIan = ContaPoupanca("Ian", 5000);
 
   List<Conta> contas = <Conta> [contaMatheus, contaRoberta];
   for (Conta conta in contas) {
@@ -14,12 +14,13 @@ void main() {
   
   contaRoberta.receber(5000);
   contaMatheus.receber(1500);
-  contaMatheus.enviar(3000);
+  contaMatheus.enviar(7000);
 
   contaWelder.valorSaldo();
-  contaWelder.receber(500);
+  contaWelder.enviar(6000);
 
   contaIan.valorSaldo();
+  contaIan.calculaRendimento();
   contaIan.enviar(100);
 
 }
