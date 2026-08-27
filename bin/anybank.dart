3f763c026cd3614b78da415970723aa4a0a0eea1
IanS04 <107213634+IanS04@users.noreply.github.com>
2026-08-26 23:20:32 -0300

    Refactor bank account instances and operations
    Removed unused Conta instances and related logic. Added ContaEmpresa and ContaInvestimento instances with corresponding operations.

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -1,20 +1,10 @@
 import 'package:anybank/conta.dart';
 
 void main() {
-  Conta contaMatheus = Conta("Matheus", 5000);
-  Conta contaRoberta = Conta("Roberta", 10000);
   ContaCorrente contaWelder = ContaCorrente("Welder", 5000);
   ContaPoupanca contaIan = ContaPoupanca("Ian", 5000);
-
-  List<Conta> contas = <Conta> [contaMatheus, contaRoberta];
-  for (Conta conta in contas) {
-    print(conta.titular);
-    print(conta.valorSaldo);
-  }
-  
-  contaRoberta.receber(5000);
-  contaMatheus.receber(1500);
-  contaMatheus.enviar(7000);
+  ContaEmpresa contaMichael = ContaEmpresa("Michael", 2500);
+  ContaInvestimento contaYuji = ContaInvestimento("Yuji", 4000);
 
   contaWelder.valorSaldo();
   contaWelder.enviar(6000);
@@ -22,5 +12,8 @@ void main() {
   contaIan.valorSaldo();
   contaIan.calculaRendimento();
   contaIan.enviar(100);
+
+  contaMichael.enviar(500);
+  contaYuji.receber(2000);
 
 }
